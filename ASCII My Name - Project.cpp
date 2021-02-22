79a5f3c85e4ad354d06e29170b46c419ee59bf13
Charles <38198248+Cinnaban@users.noreply.github.com>
2021-02-21 22:39:02 -0800

    Add files via upload
    Display your initials on the screen in block letters and create an ASCII art.

---
+++ ASCII My Name - Project.cpp
@@ -0,0 +1,24 @@
+// I am learning to code in C++ 
+
+/*
+$ g++ initials.cpp -o initials
+$ ./initials
+ .o88b. db   db  .d8b.  d8888b. db      d88888b .d8888  
+d8P  Y8 88   88 d8' `8b 88  `8D 88      88'     88'  YP 
+8P      88ooo88 88ooo88 88oobY' 88      88ooooo `8bo.   
+8b      88~~~88 88~~~88 88`8b   88      88~~~~~   `Y8b. 
+Y8b  d8 88   88 88   88 88 `88. 88booo. 88.     db   8D 
+ `Y88P' YP   YP YP   YP 88   YD Y88888P Y88888P `8888Y' 
+ */
+// The above comment is written in the command line to compile & execute initials.cpp
+
+#include <iostream>
+
+int main(){
+  std::cout << " .o88b. db   db  .d8b.  d8888b. db      d88888b .d8888  \n"; 
+  std::cout << "d8P  Y8 88   88 d8' `8b 88  `8D 88      88'     88'  YP \n";
+  std::cout << "8P      88ooo88 88ooo88 88oobY' 88      88ooooo `8bo.   \n";
+  std::cout << "8b      88~~~88 88~~~88 88`8b   88      88~~~~~   `Y8b. \n";
+  std::cout << "Y8b  d8 88   88 88   88 88 `88. 88booo. 88.     db   8D \n";
+  std::cout << " `Y88P' YP   YP YP   YP 88   YD Y88888P Y88888P `8888Y' \n";
+}
